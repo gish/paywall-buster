86338f682f60274b415b3bde84231b5fde17c597
Erik Hedberg <erik.hedberg@nordnet.se>
2017-03-19 19:23:48 +0100

    Initial commit

---
+++ buster.js
@@ -0,0 +1,34 @@
+const log = (msg) => console.log(`🐶 ${msg}`);
+
+const hasPaywall = () => {
+  return document.querySelector('#serviceplusPaywallpaywall-container');
+}
+
+const bust = () => {
+  const paywallContent = document.querySelector('.paywall-content');
+  const paywallContainer = document.querySelector('#serviceplusPaywallpaywall-container');
+  log('Barking at the paywall');
+  paywallContent.style.height = 'auto';
+  paywallContainer.remove();
+}
+
+const busted = () => {
+  const paywallContent = document.querySelector('.paywall-content');
+  return paywallContent.style.height === 'auto';
+}
+
+const runBuster = () => {
+  log('Buster is here, woof woof!');
+
+  const timer = setInterval(() => {
+    log('Running timer');
+    if (hasPaywall()) {
+      bust();
+    }
+    if (busted()) {
+      clearInterval(timer);
+    }
+  }, 1E3);
+}
+
+runBuster();
